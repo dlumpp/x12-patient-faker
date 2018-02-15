c972df69b8915566ae0687f87399940d84b60625
Dan Lumpp <danlumpp@gmail.com>
2018-02-14 22:33:32 -0500

    drop babel for webpacka ts loader

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-const a: string = 'hello world';
+import './index.html';
+import * as faker from 'faker';
+
+const output: string = faker.name.findName();
+
+const contentDiv = document.getElementById("content");
+if(contentDiv){
+    contentDiv.innerHTML = output;
+}
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        use: [{loader:'file-loader', options:{
+            name: '[name].[ext]'
+        }}]
+       ,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
